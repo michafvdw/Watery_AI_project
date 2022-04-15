8efaf0d3565ce417eaaef8e33ff52bf7147f6d90
Micha <74900539+michafvdw@users.noreply.github.com>
2022-04-15 20:15:12 +0200

    Update vegatree.js

--- libraries/vegatree.js
+++ libraries/vegatree.js
@@ -13,7 +13,7 @@ export class VegaTree {
     }    
 
     async loadConfigFile() {
-        const response = await fetch('./libraries/vegaconfig.json')
+        const response = await fetch('https://michafvdw.github.io/week9/libraries/vegaconfig.json')
         const config = await response.json()
         this.render(config)
     }
